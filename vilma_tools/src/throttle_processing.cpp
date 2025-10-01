e29f84cbd506819421c5d831d543e8afc1e6e80c
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2025-10-01 17:01:52 -0300

    Throttle parsing ok

--- vilma_tools/src/throttle_processing.cpp
+++ vilma_tools/src/throttle_processing.cpp
@@ -10,11 +10,19 @@
 #include <autoware_vehicle_msgs/msg/velocity_report.hpp>
 #include "tier4_vehicle_msgs/msg/actuation_status_stamped.hpp"
 
+#include <tf2/LinearMath/Quaternion.hpp>
+#include <tf2/LinearMath/Matrix3x3.hpp>
+
 #include "vilma_tools/vilma_ma_labeling.hpp"
 
 #include <deque>
+#include <cmath>
+// #include <iomanip>
 
 #define MA_SIZE 10
+#define BRAKE_DEADBAND 5.0
+
+#define RAD2DEG(x) ((x) * 180.0 / M_PI)
 
 class ThrottleProcessing : public rclcpp::Node
 {
@@ -41,10 +49,15 @@ public:
     longitudinal_speed_ = 0.0;
     received_new_ma_data_flg_ = false;
 
-    initial_imu_orientation_.x = ?;
-    initial_imu_orientation_.y = ?;
-    initial_imu_orientation_.z = ?;
-    initial_imu_orientation_.w = ?;
+    double roll, pitch, yaw;
+
+    initial_imu_orientation_.setX(-0.04250389939772266);
+    initial_imu_orientation_.setY(0.036776283188424894);
+    initial_imu_orientation_.setZ(0.676468632448613);
+    initial_imu_orientation_.setW(0.47291227511334805);
+
+    tf2::Matrix3x3(initial_imu_orientation_).getRPY(roll, pitch, yaw);
+    initial_imu_orientation_.setRPY(roll, pitch, 0.0);
   }
 
 private:
@@ -71,7 +84,7 @@ private:
   bool received_new_ma_data_flg_;
 
   std::deque<double> throttle_value_buffer_;
-  geometry_msgs::msg::Quaternion initial_imu_orientation_;
+  tf2::Quaternion initial_imu_orientation_;
 
   void sensors_ma_callback(const std_msgs::msg::Float64MultiArray::SharedPtr msg);
   void state_ma_callback(const std_msgs::msg::Float64MultiArray::SharedPtr msg);
@@ -90,7 +103,7 @@ void ThrottleProcessing::sensors_ma_callback(const std_msgs::msg::Float64MultiAr
     throttle_value_buffer_.pop_front();
     throttle_value_buffer_.push_back(throttle_value);
 
-    throttle_value = std::accumulate(throttle_value_buffer_.begin(), throttle_value_buffer_.end(), 0) / throttle_value_buffer_.size();
+    throttle_value = std::accumulate(throttle_value_buffer_.begin(), throttle_value_buffer_.end(), 0.0) / throttle_value_buffer_.size();
   }
   else
   {
@@ -104,7 +117,7 @@ void ThrottleProcessing::sensors_ma_callback(const std_msgs::msg::Float64MultiAr
   //* Just set throttle if brake is not pressed
   double brake_value = msg->data[SensorsMA::BRAKE_USER_PRESSURE];
 
-  if (brake_value > 5.0)
+  if (brake_value > BRAKE_DEADBAND)
   {
     received_new_ma_data_flg_ = false;
     return;
@@ -143,27 +156,75 @@ void ThrottleProcessing::state_ma_callback(const std_msgs::msg::Float64MultiArra
 
 void ThrottleProcessing::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
 {
-  // TODO Compensar pitch com calibração inicial
-
-  geometry_msgs::msg::Quaternion orientation;
-
-  pitch_angle_ = ?;
-
-  // TODO Obter aceleração longitudinal
-
-  longitudinal_acceleration_ = ?;
-
+  imu_msg_ = *msg;
 }
 
 void ThrottleProcessing::publish_data()
 {
+
+  //* IMU processing
+
+  //* > Compensar pitch com calibração inicial
+
+  tf2::Quaternion orientation;
+
+  double roll, pitch, yaw;
+
+  // Current orientation
+  orientation.setX(imu_msg_.orientation.x);
+  orientation.setY(imu_msg_.orientation.y);
+  orientation.setZ(imu_msg_.orientation.z);
+  orientation.setW(imu_msg_.orientation.w);
+
+  tf2::Matrix3x3(orientation).getRPY(roll, pitch, yaw);
+  orientation.setRPY(roll, pitch, 0.0);
+
+  tf2::Matrix3x3(initial_imu_orientation_).getRPY(roll, pitch, yaw);
+
+  RCLCPP_INFO(this->get_logger(), "       Ref | roll: %lf, pitch: %lf, yaw: %lf ", RAD2DEG(roll), RAD2DEG(pitch), RAD2DEG(yaw));
+
+  tf2::Matrix3x3(orientation).getRPY(roll, pitch, yaw);
+
+  RCLCPP_INFO(this->get_logger(), "  Measured | roll: %lf, pitch: %lf, yaw: %lf ", RAD2DEG(roll), RAD2DEG(pitch), RAD2DEG(yaw));
+  // RCLCPP_INFO(this->get_logger(), "Before | x: %lf, y: %lf, z: %lf , w: %lf ", imu_msg_.orientation.x, imu_msg_.orientation.y, imu_msg_.orientation.z, imu_msg_.orientation.w);
+
+  // Current orientation transformed to base_link
+  orientation = initial_imu_orientation_.inverse() * orientation;
+
+  orientation.normalize();
+
+  tf2::Matrix3x3(orientation).getRPY(roll, pitch, yaw);
+
+  RCLCPP_INFO(this->get_logger(), "Calibrated | roll: %lf, pitch: %lf, yaw: %lf ", RAD2DEG(roll), RAD2DEG(pitch), RAD2DEG(yaw));
+
+  pitch_angle_ = RAD2DEG(pitch);
+
+  //* > Getting longitudinal acceleration
+
+  tf2::Vector3 linear_acceleration;
+
+  linear_acceleration.setX(imu_msg_.linear_acceleration.x);
+  linear_acceleration.setY(imu_msg_.linear_acceleration.y);
+  linear_acceleration.setZ(imu_msg_.linear_acceleration.z);
+
+  // Transforming linear acceleration to base_link frame
+  linear_acceleration = tf2::Matrix3x3(initial_imu_orientation_).inverse() * linear_acceleration;
+
+  // Getting longitudinal acceleration as the acceleration in the plane, once the vehicle is driving in straight line.
+  longitudinal_acceleration_ = sqrt(linear_acceleration.getX() * linear_acceleration.getX() + linear_acceleration.getY() * linear_acceleration.getY());
+
+  //* Data publishing
+
   tier4_vehicle_msgs::msg::ActuationStatusStamped actuation_msg;
   autoware_vehicle_msgs::msg::SteeringReport steering_msg;
   autoware_vehicle_msgs::msg::VelocityReport velocity_msg;
   std_msgs::msg::Float32 pitch_msg;
   sensor_msgs::msg::Imu imu_msg;
 
-  actuation_msg.status.accel_status = throttle_value_ / 100.0;
+  actuation_msg.status.accel_status = throttle_value_;
+
+
+  RCLCPP_INFO(this->get_logger(), "Filtered throttle: %lf", actuation_msg.status.accel_status);
   actuation_status_pub_->publish(actuation_msg);
 
   steering_msg.steering_tire_angle = steering_angle_;
